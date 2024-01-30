b2bbd87109a1697d42826f42a35bb2851d6d3314
KHalil <khalilziadeh98@gmail.com>
2024-01-30 08:08:02 +0200

    enhanced responsivness

--- static/JS/reveal.js
+++ static/JS/reveal.js
@@ -1,11 +1,15 @@
 let checkBtn = document.querySelector("header ul li:first-child button");
 checkBtn.addEventListener("click", () => {
   document.querySelector(".search").style.display = "block";
+  document.querySelector(".search").style.order = "2";
   document.querySelector("#card").style.display = "none";
+  document.querySelector("#card").style.order = "3";
 });
 
 let reservBtn = document.querySelector("header ul li:last-child button");
 reservBtn.addEventListener("click", () => {
   document.querySelector(".search").style.display = "none";
+  document.querySelector(".search").style.order = "3";
   document.querySelector("#card").style.display = "block";
+  document.querySelector("#card").style.order = "2";
 });
